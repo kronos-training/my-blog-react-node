9519154e778be8acdd4d304674117049a119b202
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2019-09-07 21:18:31 -0500

    displaying comments

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
@@ -32,6 +33,8 @@ const ArticlePage = ({ match }) => {
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+
+            <CommentsList comments={articleInfo.comments} />
             
             <h3>Other Article</h3>
             <ArticlesList articles={otherArticles} />
